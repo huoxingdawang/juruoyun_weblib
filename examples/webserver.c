f79a28dc78698ab4213c648ddc6f3fc0f893c7d1
火星大王 <lijunyandeyouxiang@163.com>
2020-07-23 22:45:12 +0800

    修复request head解析多一个\r的问题

--- examples/webserver.c
+++ examples/webserver.c
@@ -34,19 +34,38 @@ int main(int argc,char** argv)
 		jwl_http_reqh * reqh=jwl_http_reqh_decode(get,NULL);
 		jwl_http_reqh_view(reqh);		
 		
-		
-		
-		
-		
 		resh=jwl_http_resh_new();
+		
+		
+		
+		
 		if(jbl_string_space_ship_chars(reqh->url,"/favicon.ico")==0)
 		{
-			resh=jwl_http_resh_set_status		(resh,200);
-			resh=jwl_http_resh_set_cache		(resh,JWL_HTTP_CACHE_NO);
 			resh=jwl_http_resh_set_content_type	(resh,jbl_string_cache_get(UC JWL_HTTP_CONTENT_TYPE_ICO));
-			resh=jwl_http_resh_set_charset		(resh,JWL_HTTP_CHARSET_UTF8);
-			resh=jwl_http_resh_set_etag			(resh,jbl_string_cache_get(UC"456"));
-			FILE *fp;res=jbl_string_add_file(res,fp=fopen("testfiles//logo.ico","rb"));fclose(fp);
+			resh=jwl_http_resh_set_cache		(resh,JWL_HTTP_CACHE_MAX_AGE);
+			resh=jwl_http_resh_set_cache_max_age(resh,3600);
+			resh=jwl_http_resh_set_etag			(resh,jbl_string_cache_get(UC"456"));\
+			if(jbl_string_space_ship_chars(reqh->etag,"456")==0)
+				resh=jwl_http_resh_set_status		(resh,304);				
+			else
+			{
+				resh=jwl_http_resh_set_status		(resh,200);
+				FILE *fp;res=jbl_string_add_file(res,fp=fopen("testfiles//logo.ico","rb"));fclose(fp);
+			}
+		}
+		else if(jbl_string_space_ship_chars(reqh->url,"/pic")==0)
+		{
+			resh=jwl_http_resh_set_content_type	(resh,jbl_string_cache_get(UC JWL_HTTP_CONTENT_TYPE_PNG));
+			resh=jwl_http_resh_set_cache		(resh,JWL_HTTP_CACHE_MAX_AGE);
+			resh=jwl_http_resh_set_cache_max_age(resh,3600);
+			resh=jwl_http_resh_set_etag			(resh,jbl_string_cache_get(UC"789"));
+			if(jbl_string_space_ship_chars(reqh->etag,"789")==0)
+				resh=jwl_http_resh_set_status		(resh,304);
+			else
+			{
+				resh=jwl_http_resh_set_status		(resh,200);
+				FILE *fp;res=jbl_string_add_file(res,fp=fopen("testfiles//test.png","rb"));fclose(fp);
+			}
 		}
 		else
 		{
